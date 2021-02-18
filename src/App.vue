677abc54e55dc70e90f6910eeec517126dfebef0
nadavmozes <61967076+nadavmozes@users.noreply.github.com>
2021-02-18 14:54:27 +0200

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="home">
+    <app-header></app-header>
+    <intro></intro>
+    <features></features>
+    <productive></productive>
+    <team></team>
+    <newsletter></newsletter>
+    <page-footer></page-footer>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import AppHeader from "@/cmps/AppHeader.vue";
+import Intro from "@/cmps/Intro";
+import Features from "@/cmps/Features";
+import Productive from "@/cmps/Productive.vue";
+import Team from "@/cmps/Team.vue";
+import Newsletter from "@/cmps/Newsletter.vue";
+import PageFooter from "@/cmps/PageFooter.vue";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+export default {
+  name: "Home",
+  components: {
+    AppHeader,
+    Intro,
+    Features,
+    Productive,
+    Team,
+    Newsletter,
+    PageFooter,
+  },
+};
+</script>
